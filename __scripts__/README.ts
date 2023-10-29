860f6996526f1b74901e01590dd319a5b97849bc
Tim <aitimate@dalveytech.com>
2023-10-29 16:32:38 +0800

    add: readme script

---
+++ __scripts__/README.ts
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+
+const source = path.join(process.cwd(), "README.md");
+const dest = path.join(process.cwd(), "dist", "README.md");
+
+// @ts-ignore
+fs.copyFile(source, dest, (err) => {
+    if (err) throw err;
+    console.log(`${source} was copied to ${dest}`);
+});
